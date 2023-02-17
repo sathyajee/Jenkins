1b041a9c70a980301c51a435dd7e5d8d69e6c7d9
Sathyajeeth R <ravi.satyajeeth@gmail.com>
2023-02-17 19:22:35 +0530

    Second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "My srn is PES1UG20CS686\n";
+  cout << "My name is Sathyajeeth R";
   return 0;
 }
